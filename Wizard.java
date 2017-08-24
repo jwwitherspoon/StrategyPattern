9720d29be1879215ccc09d9b6b18b1156b5b5f3c
jwwitherspoon <jwwitherspoon@gmail.com>
2017-08-24 15:27:38 -0400

    Update Wizard.java
    Add comments

--- Wizard.java
+++ Wizard.java
@@ -1,11 +1,14 @@
-
+// Subclass of Character
 public class Wizard extends Character {
 	
+	// Explicit constructor that specifies which groups the character falls into
+	// by instantiating classes that implement HeightGroup and AbilityGroup
 	public Wizard() {
 		heightGroup = new Tall();
 		abilityGroup = new Magic();
 	}
 	
+	// Displays character type
 	public void displayType() {
 		System.out.println("I am a wizard.");
 	}
