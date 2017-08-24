f40ea990af98d4dc1e22104ed1dcc1f6fe87f44e
jwwitherspoon <jwwitherspoon@gmail.com>
2017-08-24 15:23:13 -0400

    Update Dwarf.java
    Add comments

--- Dwarf.java
+++ Dwarf.java
@@ -1,11 +1,14 @@
-
+// Subclass of Character
 public class Dwarf extends Character {
 	
+	// Explicit constructor that specifies which groups the character falls into
+	// by instantiating classes that implement HeightGroup and AbilityGroup
 	public Dwarf() {
 		heightGroup = new Short();
 		abilityGroup = new Hammer();
 	}
 	
+	// Displays character type
 	public void displayType() {
 		System.out.println("I am a dwarf.");
 	}
