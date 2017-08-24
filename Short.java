6c1847fe5dce94109a4dd1a4b8fe02624847d033
jwwitherspoon <jwwitherspoon@gmail.com>
2017-08-24 15:26:14 -0400

    Update Short.java
    Add comments

--- Short.java
+++ Short.java
@@ -1,6 +1,9 @@
-
+// Class that defines specific height group
 public class Short implements HeightGroup {
+	
+	// Prints that the character is short
 	public void height() {
 		System.out.println("I am short.");
 	}
+	
 }
