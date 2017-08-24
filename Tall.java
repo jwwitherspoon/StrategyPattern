762cac23c8b4859818ea66cfa3399fd2cefc36c2
jwwitherspoon <jwwitherspoon@gmail.com>
2017-08-24 15:27:08 -0400

    Update Tall.java
    Add comments

--- Tall.java
+++ Tall.java
@@ -1,6 +1,9 @@
-
+// Class that defines specific height group
 public class Tall implements HeightGroup {
+	
+	// Prints that the character is tall
 	public void height() {
 		System.out.println("I am tall.");
 	}
+	
 }
