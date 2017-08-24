9e0ab00fd1de392b1e49f73e10b4605453094f95
jwwitherspoon <jwwitherspoon@gmail.com>
2017-08-24 15:25:38 -0400

    Update Magic.java
    Add comments

--- Magic.java
+++ Magic.java
@@ -1,6 +1,9 @@
-
+// Class that defines specific ability group
 public class Magic implements AbilityGroup {
+	
+	// Prints that the character's ability is magic
 	public void ability() {
 		System.out.println("I am good with magic.");
 	}
+	
 }
