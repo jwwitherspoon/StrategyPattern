e9421e8d7a30091ebb1c41b128a00bb842f2da5a
jwwitherspoon <jwwitherspoon@gmail.com>
2017-08-24 15:24:54 -0400

    Update Hobbit.java
    Add comments

--- Hobbit.java
+++ Hobbit.java
@@ -1,11 +1,14 @@
-
+// Subclass of Character
 public class Hobbit extends Character {
 	
+	// Explicit constructor that specifies which groups the character falls into
+	// by instantiating classes that implement HeightGroup and AbilityGroup
 	public Hobbit() {
 		heightGroup = new Short();
 		abilityGroup = new Sword();
 	}
 	
+	// Displays character type
 	public void displayType() {
 		System.out.println("I am a hobbit.");
 	}
