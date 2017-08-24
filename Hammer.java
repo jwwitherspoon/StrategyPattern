b6e526fa5e8d91df46d3d3228dc638508e0f92ea
jwwitherspoon <jwwitherspoon@gmail.com>
2017-08-24 15:24:01 -0400

    Update Hammer.java
    Add comments

--- Hammer.java
+++ Hammer.java
@@ -1,6 +1,9 @@
-
+// Class that defines specific ability group
 public class Hammer implements AbilityGroup {
+	
+	// Prints that the character's ability is a hammer
 	public void ability() {
 		System.out.println("I am good with a hammer.");
 	}
+	
 }
