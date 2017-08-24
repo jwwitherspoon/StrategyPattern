4d8a58d2341927e2bf235842ccc816094909fc41
jwwitherspoon <jwwitherspoon@gmail.com>
2017-08-24 15:22:38 -0400

    Update Character.java
    Add comments

--- Character.java
+++ Character.java
@@ -1,16 +1,22 @@
-
+// Base class for all the characters
 public abstract class Character {
+	
+	// Define parameters from HeightGroup and AbilityGroup interfaces
 	HeightGroup heightGroup;
 	AbilityGroup abilityGroup;
 	
+	// Explicit constructor
 	public Character() {}
 	
+	// Declare abstract method to display the type of each different subclass
 	abstract void displayType();
 	
+	// Delegate display of height to a class that implements HeightGroup interface
 	public void displayHeight() {
 		heightGroup.height();
 	}
 	
+	// Delegate display of ability to a class that implements AbilityGroup interface
 	public void displayAbility() {
 		abilityGroup.ability();
 	}
