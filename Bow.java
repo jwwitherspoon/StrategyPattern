90ede51a15af4646e0e6e352da46b83b1300def4
jwwitherspoon <jwwitherspoon@gmail.com>
2017-08-24 15:22:07 -0400

    Update Bow.java
    Add comments

--- Bow.java
+++ Bow.java
@@ -1,6 +1,9 @@
-
+// Class that defines specific ability group
 public class Bow implements AbilityGroup {
+	
+	// Prints that the character's ability is a bow
 	public void ability() {
 		System.out.println("I am good with a bow.");
 	}
+	
 }
