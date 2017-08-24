06547678a139f8036c80d178578ba6177bca0e03
jwwitherspoon <jwwitherspoon@gmail.com>
2017-08-24 15:26:40 -0400

    Update Sword.java
    Add comments

--- Sword.java
+++ Sword.java
@@ -1,6 +1,9 @@
-
+// Class that defines specific ability group
 public class Sword implements AbilityGroup {
+	
+	// Prints that the character's ability is a sword
 	public void ability() {
 		System.out.println("I am good with a sword.");
 	}
+	
 }
