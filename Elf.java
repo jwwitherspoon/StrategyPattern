fc28a46a5c8ece2936b09c8313dbcfb4ebed0a21
jwwitherspoon <jwwitherspoon@gmail.com>
2017-08-24 15:23:37 -0400

    Update Elf.java
    Add comments

--- Elf.java
+++ Elf.java
@@ -1,11 +1,14 @@
-
+// Subclass of Character
 public class Elf extends Character {
 	
+	// Explicit constructor that specifies which groups the character falls into
+	// by instantiating classes that implement HeightGroup and AbilityGroup
 	public Elf() {
 		heightGroup = new Tall();
 		abilityGroup = new Bow();
 	}
 	
+	// Displays character type
 	public void displayType() {
 		System.out.println("I am an elf.");
 	}
