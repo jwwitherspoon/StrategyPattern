c994c74fc589d8927ca888d230dbe38885e41fd8
jwwitherspoon <jwwitherspoon@gmail.com>
2017-08-24 15:25:15 -0400

    Update Human.java
    Add comments

--- Human.java
+++ Human.java
@@ -1,11 +1,14 @@
-
+// Subclass of Character
 public class Human extends Character {
 	
+	// Explicit constructor that specifies which groups the character falls into
+	// by instantiating classes that implement HeightGroup and AbilityGroup
 	public Human() {
 		heightGroup = new Tall();
 		abilityGroup = new Sword();
 	}
 	
+	// Displays character type
 	public void displayType() {
 		System.out.println("I am a human.");
 	}
